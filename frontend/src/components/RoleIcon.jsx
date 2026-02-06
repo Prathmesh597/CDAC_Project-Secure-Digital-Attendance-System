bb7d22efaab36417186ae009ec8f5b00f7bee3be
Prathmesh597 <prathmesh010102@gmail.com>
2026-02-06 17:13:49 +0530

    added download attendance report

--- frontend/src/components/RoleIcon.jsx
+++
@@ -1,9 +0,0 @@
-import { styled } from '@mui/material';
-
-// This component is a styled 'img' element to display role icons
-const RoleIcon = styled('img')({
-  width: 40,
-  height: 40,
-});
-
-export default RoleIcon;
